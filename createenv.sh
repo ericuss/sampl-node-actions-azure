82443854e61b40091832a4dcecde0ac92bf92a9f
Ericuss <etorreg@gmail.com>
2021-06-01 22:16:05 +0200

    create app service sh

---
+++ createenv.sh
@@ -0,0 +1,12 @@
+# az login
+
+az appservice plan create \
+    --resource-group rg-test-app-node-actions-azure \
+    --name sp-test-app-node-actions-azure \
+    --is-linux
+
+az webapp create \
+    --name test-app-node-actions-azure-dev \
+    --plan sp-test-app-node-actions-azure \
+    --resource-group rg-test-app-node-actions-azure \
+    --runtime "node|10.14"
